ad2d9ec94ea12005b8e6c9955a8e61c3adc5a008
spilledyear <spilledyear@outlook.com>
2019-06-12 20:38:48 +0800

    修改注册中心

--- src/main/java/com/sxy/sdubbo/SDubboApplication.java
+++ src/main/java/com/sxy/sdubbo/SDubboApplication.java
@@ -25,9 +25,9 @@ public class SDubboApplication {
         @Bean
         public RegistryConfig registryConfig() {
             RegistryConfig registryConfig = new RegistryConfig();
-            registryConfig.setAddress("nacos://10.9.44.133:8848");
+//            registryConfig.setAddress("nacos://10.9.44.133:8848");
 //            registryConfig.setAddress("multicast://224.5.6.7:1234");
-//            registryConfig.setAddress("zookeeper://10.9.15.32:2181");
+            registryConfig.setAddress("zookeeper://10.9.44.133:2181");
 
             // 注册简化版的的url到注册中心
             registryConfig.setSimplified(true);
@@ -38,8 +38,8 @@ public class SDubboApplication {
         @Bean
         public MetadataReportConfig metadataReportConfig() {
             MetadataReportConfig metadataReportConfig = new MetadataReportConfig();
-            metadataReportConfig.setAddress("nacos://10.9.44.133:8848");
-//            metadataReportConfig.setAddress("zookeeper://10.9.15.32:2181");
+//            metadataReportConfig.setAddress("nacos://10.9.44.133:8848");
+            metadataReportConfig.setAddress("zookeeper://10.9.44.133:2181");
 //            metadataReportConfig.setAddress("redis://localhost:6379");
             return metadataReportConfig;
         }
@@ -48,8 +48,8 @@ public class SDubboApplication {
         @Bean
         public ConfigCenterConfig configCenterConfig() {
             ConfigCenterConfig configCenterConfig = new ConfigCenterConfig();
-            configCenterConfig.setAddress("nacos://10.9.44.133:8848");
-//            configCenterConfig.setAddress("zookeeper://127.0.0.1:2181");
+//            configCenterConfig.setAddress("nacos://10.9.44.133:8848");
+            configCenterConfig.setAddress("zookeeper://10.9.44.133:2181");
             return configCenterConfig;
         }
     }
